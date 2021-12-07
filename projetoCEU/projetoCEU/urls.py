e9e43e318dbafda7c1293ac47100492e2f6a3da4
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2021-12-07 18:09:48 -0300

    Finalização do template do sidebar e de boa parte das páginas, principalmente login

--- projetoCEU/projetoCEU/urls.py
+++ projetoCEU/projetoCEU/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('login/', include('home.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('cadastro/', include('cadastro.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
