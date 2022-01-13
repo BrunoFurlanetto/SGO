b8527fb56c1cafb2ceac8db8409c913ceb272a5a
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-01-13 16:28:23 -0300

    Algumas alterações feitas ao entrar com o computador do CEU pela primeira vez

--- projetoCEU/cadastro/views.py
+++ projetoCEU/cadastro/views.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import OrdemDeServico, Professores, Atividades, Tipo
