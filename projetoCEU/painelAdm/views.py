c9df30bdc216c3bfbfa411786cb1a64e2d214892
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-02-02 17:19:08 -0300

    Gáfico de atividades, diárias e horas do mês pronto

--- projetoCEU/painelAdm/views.py
+++ projetoCEU/painelAdm/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from cadastro.models import Professores
+from painelAdm.funcoes import contar_atividades, contar_horas, contar_diaria
 
 
 def painelGeral(request):
-    return render(request, 'paineladm/painelGeral.html')
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    professores = Professores.objects.all()
+
+    for professor in professores:
+        professor.n_atividades = contar_atividades(professor)
+        professor.n_horas = contar_horas(professor)
+        professor.n_diaria = contar_diaria(professor)
+
+    return render(request, 'paineladm/painelGeral.html', {'professores': professores})
