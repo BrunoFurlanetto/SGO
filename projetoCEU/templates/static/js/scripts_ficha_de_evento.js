307ad42f63b6a9b2b8926cc71d62c55d652ee60b
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2022-07-23 14:48:47 -0300

    Lista de segurados nas informações adicionais da FE ok

--- projetoCEU/templates/static/js/scripts_ficha_de_evento.js
+++ projetoCEU/templates/static/js/scripts_ficha_de_evento.js
@@ -415,6 +415,8 @@ function edita_ficha(){
     $('.conteudo-ficha').removeClass('ver-conteudo-ficha')
 
     $('.perfil-participantes-ver-ficha, .observacoes-ver-ficha, #ceu :first-child, #atividades_ceu_ver_ficha, #locacoes_ceu_ver_ficha').removeClass('none')
-    $('.ceu, #atividades_peraltas_ver_ficha, #atividades_eco_ver_ficha, .peraltas :first-child, .peraltas').removeClass('none')
-    $('#lista_locacoes_ceu, #lista_atividades_ceu, #lista_atividades_peraltas, #lista_atividades_eco').addClass('none')
+    $('.ceu, #atividades_peraltas_ver_ficha, #atividades_eco_ver_ficha, .peraltas :first-child, .peraltas, #arquivo_atual_lista_editar').removeClass('none')
+    $('#lista_locacoes_ceu, #lista_atividades_ceu, #lista_atividades_peraltas, #lista_atividades_eco, #arquivo_atual_lista').addClass('none')
+
+    lista_segurados()
 }
