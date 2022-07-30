50c0954b6024f8c74e48387dd717caa61a776ee0
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2022-07-30 13:48:25 -0300

    Migração do arquivo local_settings.py pro servidor

--- projetoCEU/projetoCEU/settings.py
+++ projetoCEU/projetoCEU/settings.py
@@ -92,11 +92,8 @@ WSGI_APPLICATION = 'projetoCEU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'user_ceu',
-        'PASSWORD': 'c4^$^3Ey*5rN',
-        'HOST': '127.0.0.1',
-        'NAME': 'ceu'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
