57894ce5234670db90ab74c321eaa94894beca23
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2023-04-23 02:25:18 -0300

    Alteração das migrations

--- ordemDeServico/migrations/0019_auto_20230421_0029.py
+++ ordemDeServico/migrations/0019_auto_20230421_0029.py
@@ -1,18 +1,4 @@
-from django.db import migrations
-
-from ordemDeServico.models import OrdemDeServico, DadosTransporte
-
-
-def popular_dados_transporte_e_monitor_embarque(apps, schema_editor):
-    ordens = OrdemDeServico.objects.all()
-
-    for ordem in ordens:
-        if ordem.dados_transporte:
-            transporte = DadosTransporte.objects.get(id=ordem.dados_transporte.id)
-            transporte.monitor_embarque = ordem.monitor_embarque
-            transporte.save()
-            ordem.dados_transporte_temp.set([ordem.dados_transporte.id])
-
+from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
@@ -21,5 +7,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(popular_dados_transporte_e_monitor_embarque),
     ]
