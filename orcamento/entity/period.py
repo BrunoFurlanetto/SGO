29319787a2630cb716442debf15955b85af0aa74
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2023-12-04 10:38:11 -0300

    - zerar o Period(), afim de não interferir no valor
    - revisão de calculos para apresentação

--- orcamento/entity/period.py
+++ orcamento/entity/period.py
@@ -4,7 +4,7 @@ from .basevalue import BaseValue
 class Period(BaseValue):
     def __init__(self, periods):
         self.periods = periods
-        values = [float(period.valor) for period in periods]
+        values = [float(0) for period in periods]
         super().__init__(values=values)
 
     def get_periods(self):
