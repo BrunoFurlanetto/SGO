d9d5e1a12c0e0ff68970c640a9f758ca31a3e89d
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2024-02-20 17:28:03 -0300

    Correção digitação nas classes de transport

--- orcamento/entity/interface/transport_protocol.py
+++ orcamento/entity/interface/transport_protocol.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractclassmethod
 from ..basevalue import BaseValue
 
+
 class TransportProtocol(ABC, BaseValue):
     def __init__(self, values, periods, days):
         super().__init__(values)
-        self.periods=periods
-        self.days=days
+        self.periods = periods
+        self.days = days
         self.min_payers = 30
-    
+
     def set_min_payers(self, min_payers):
         self.min_payers = min_payers
-        return self.min_payers        
+        return self.min_payers
 
     @abstractclassmethod
     def calc_value_transport(self, is_transport):
