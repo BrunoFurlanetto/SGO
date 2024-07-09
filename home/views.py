de3a8d21107adc0d0ff6d3e2d423de7daba80b8d
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2024-07-09 18:53:19 -0300

    Correção handler 500

--- home/views.py
+++ home/views.py
@@ -44,5 +44,5 @@ def handler404(request, exception):
     return render(request, 'home/404.html')
 
 
-def handler500(request, exception):
+def handler500(request):
     return render(request, 'home/500.html', status=500)
