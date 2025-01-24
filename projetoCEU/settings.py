d88665cef284d37b08281535d16f08493f4d6c52
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-01-24 11:22:48 -0300

    Remoção da configuração do cash

--- projetoCEU/settings.py
+++ projetoCEU/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
 STATIC_ROOT = os.path.join('static')
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
