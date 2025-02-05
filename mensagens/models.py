eb96f5f174b739d4be69299d75781f93b35cbf13
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-02-04 21:22:03 -0300

    Model mensagem criada

--- mensagens/models.py
+++ mensagens/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Mensagem(models.Model):
+    remetente = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='remetente')
+    destinatario = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='destinatario')
+    nome_remetente = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text='Salva o nome do remetente no caso do usuário ser excluido'
+    )
+    nome_destinatario = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text='Salva o nome do destinatário no caso do usuário ser excluido',
+    )
+    conteudo = models.TextField()
+    anexo = models.FileField(upload_to='mensagens/arquivos/', blank=True, null=True)
+    data_hora_envio = models.DateTimeField(auto_now_add=True)
+    data_hora_leitura = models.DateTimeField(blank=True, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f'Mensagem entre {self.remetente} e {self.destinatario} relacionado ao {self.content_type.model}'
+
+    def save(self, *args, **kwargs):
+        if not self.nome_remetente and self.remetente:
+            self.nome_remetente = self.remetente.get_full_name() or self.remetente.username
+
+        if not self.nome_destinatario and self.destinatario:
+            self.nome_destinatario = self.destinatario.get_full_name() or self.destinatario.username
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.anexo:
+            self.anexo.delete()
+
+        super().delete(*args, **kwargs)
+
