36e5662a8f4832dd3eec9e2ea190d1fded806c50
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2025-04-22 14:41:55 -0300

    Correção aparição tabela de totalização na ficha financeira

--- financeiro/views.py
+++ financeiro/views.py
@@ -150,7 +150,7 @@ def revisar_ficha_financeira(request, id_ficha_financeira):
         'planos_pagamento': cadastro_planos_pagamento,
         'nota_fiscal': cadastro_nota_fiscal,
         'revisando': True,
-        'faturando': User.objects.filter(pk=request.user.id, groups__name__in=['Financeiro']).exists(),
+        'financeiro': User.objects.filter(pk=request.user.id, groups__name__in=['Financeiro']).exists(),
         'pagamento_eficha': ficha.planos_pagamento.verficar_eficha(),
         'telefone_financeiro': responsavel_financeiro.fone,
         'whats_financeiro': responsavel_financeiro.whats or '',
